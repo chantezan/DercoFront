bbf334be5a0736c56116a67da03912aa3854f439
poblete <pablo.poblete@ing.uchile.cl>
2022-01-28 10:21:50 -0300

    arreglo en axios

--- static/App.vue
+++ static/App.vue
@@ -16,7 +16,7 @@ export default {
   mounted () {
     axios
       .get('https://bs8e9e1s09.execute-api.us-east-1.amazonaws.com/pruebaDerco5')
-      .then(response => (this.info = response))
+      .then(response => (this.info = response.data))
   }
 };
 </script>
