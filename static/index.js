8f5105c30b5c9de0b17e67662675999ad8dc38e5
poblete <pablo.poblete@ing.uchile.cl>
2022-08-08 00:59:12 -0400

    example

--- static/index.js
+++ static/index.js
@@ -1,14 +1,13 @@
-import Vue from 'vue';
-import App from './App.vue'
-import Amplify from 'aws-amplify';
-import '@aws-amplify/ui-vue';
-
-Vue.config.productionTip = false
-
-new Vue({
+import { createApp } from 'vue'
+import Cripto from './Cripto'
+// Creamos una instancia de la aplicación Vue
+const app = createApp({
+  // Elemento html donde se va ha renderizar el contenido
   el: '#app',
-  template: '<App/>',
-  components: { 
-    App
-  }
+  // Activamos el componente dentro de la app
+  components : {
+    Cripto,
+  },
 })
+// Montamos la app en el div #app de nuestra plantilla index.html.
+app.mount('#app')
